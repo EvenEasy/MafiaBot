889194bf8a91ad2045ec61e89d772be57db6a26c
EvenEasy <75838378+EvenEasy@users.noreply.github.com>
2022-05-11 16:37:37 +0300

    Update Main.py

--- MafiaBot/Main.py
+++ MafiaBot/Main.py
@@ -7,6 +7,8 @@ from discord.ext import commands
 from discord.utils import get
 import random
 
+token = "OTUxNTQ0ODI5MDg1NjMwNDc0.GNnQtQ.Vxe_smowF0ZZWUkmMxGOY-SwUpA1DyRJrd3_tQ"
+
 client = commands.Bot(command_prefix='$')
 
 @client.event
